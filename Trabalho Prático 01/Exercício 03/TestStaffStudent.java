cc890ef8039ca23ce6e388be57444e80fd59a9ed
Giuliana Ferreira Chantal <103539919+giuliana-chantal@users.noreply.github.com>
2024-09-23 16:30:07 -0300

    Create TestStaffStudent.java

---
+++ Trabalho Prático 01/Exercício 03/TestStaffStudent.java
@@ -0,0 +1,9 @@
+public class TestStaffStudent {
+    public static void main(String[] args) {
+        Staff staff = new Staff("Giuliana", "Professor");
+        Student student = new Student("Chantal", "Computer Science");
+
+        System.out.println(staff);
+        System.out.println(student);
+    }
+}
