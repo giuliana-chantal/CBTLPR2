956e9b11f1395994c5afc50c8b06c441aec0766d
Giuliana Ferreira Chantal <103539919+giuliana-chantal@users.noreply.github.com>
2024-09-23 16:21:18 -0300

    Create Main.java

---
+++ Trabalho Prático 01/Exercício 01/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Wellington Tuler", "tulermoraes@yahoo.com", 'm');
+        System.out.println(author);
+    }
+}
