0c8833f41c29ad959fdde59b76f0b67646436353
Giuliana Ferreira Chantal <103539919+giuliana-chantal@users.noreply.github.com>
2024-12-13 14:20:37 -0300

    Create Aluno.java

---
+++ Trabalho Prático 04 /Aluno.java
@@ -0,0 +1,10 @@
+// Giuliana Ferreira Chantal - CB3013171
+
+public class Aluno {
+    private int id;
+    private String nome;
+    private int idade;
+    private String curso;
+
+    // Construtores, getters e setters
+}
